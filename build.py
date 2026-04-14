81d792a881a9a548a02b1001b9e14d0314eac57c
alybrewer <21brewera@gmail.com>
2026-04-13 20:57:42 -0400

    deploy attempt 2

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 from flask_frozen import Freezer
 from app import app
 
+# This helps Frozen-Flask find your files in the GitHub environment
+app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_DESTINATION'] = 'build'
+
 freezer = Freezer(app)
 
 if __name__ == '__main__':
+    # We use 'ignore' so that if a tab link is weird, it doesn't kill the build
     freezer.freeze()
-
-#testing
